26673fad81ba24c6fdf5b210bc05e00f044a9e2a
Yuliia Nykanorova <juliyasos@ukr.net>
2025-01-18 17:06:11 +0200

    added owl carousel for skills section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-import carousel from "./carousel";
+// owl-carousel;
 
-carousel();
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true, 
+        margin: 10, 
+        nav: true, 
+        responsive: {
+        0: { items: 2 }, 
+        576: { items: 3 }, 
+        768: { items: 4 }, 
+        992: { items: 5 }  
+        }
+    });
+});
+
+
